ff2b17b4a1ac5604d42f892189b0b3f202b78963
sciNtech <sntpresident@gmail.com>
2018-08-17 20:44:49 +0800

    1.1: Updated Integration section

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const productSection = document.getElementById('product')
     const integrationSection = document.getElementById('integration')
 
+    const python = document.getElementById('python')
+    const javascript = document.getElementById('javascript')
+    const node = document.getElementById('node')
+    const java = document.getElementById('java')
+    const php = document.getElementById('php')
+
+    // Set default language shown
+    python.style.color = '#222f3e'
+
+    const editor = document.querySelector('#code-editor p')
+
     productOption.onclick = () => {
         productSection.scrollIntoView({
             behavior: 'smooth'
@@ -17,5 +28,49 @@ document.addEventListener('DOMContentLoaded', () => {
         integrationSection.scrollIntoView({
             behavior: 'smooth'
         })
+    }
+
+    python.onclick = () => {
+        python.style.color = '#222f3e'
+        javascript.style.color = '#fdcb6e'
+        node.style.color = '#fdcb6e'
+        java.style.color = '#fdcb6e'
+        php.style.color = '#fdcb6e'
+
+        showEditor('python')
+    } 
+    javascript.onclick = () => {
+        python.style.color = '#fdcb6e'
+        javascript.style.color = '#222f3e'
+        node.style.color = '#fdcb6e'
+        java.style.color = '#fdcb6e'
+        php.style.color = '#fdcb6e'
+    }
+    node.onclick = () => {
+        python.style.color = '#fdcb6e'
+        javascript.style.color = '#fdcb6e'
+        node.style.color = '#222f3e'
+        java.style.color = '#fdcb6e'
+        php.style.color = '#fdcb6e'
+    }
+    java.onclick = () => {
+        python.style.color = '#fdcb6e'
+        javascript.style.color = '#fdcb6e'
+        node.style.color = '#fdcb6e'
+        java.style.color = '#222f3e'
+        php.style.color = '#fdcb6e'
+    }
+    php.onclick = () => {
+        python.style.color = '#fdcb6e'
+        javascript.style.color = '#fdcb6e'
+        node.style.color = '#fdcb6e'
+        java.style.color = '#fdcb6e'
+        php.style.color = '#222f3e'
+    }
+
+    function showEditor(lang) {
+        if (lang == 'python') {
+
+        }
     }
 })
